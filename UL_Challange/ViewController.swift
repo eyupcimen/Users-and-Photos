42cdf320d8c008ae4f4891f353583fcab309b80b
eyupcimen <cmneyp@gmail.com>
2020-01-17 17:52:28 +0300

    Initial Commit

---
+++ UL_Challange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UL_Challange
+//
+//  Created by eyup cimen on 17.01.2020.
+//  Copyright © 2020 eyup cimen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
